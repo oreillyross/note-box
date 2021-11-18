f1c79e98e0c5c288bd6552c5ce9916d205db4e1d
Faktor 10 <oreillyross@gmail.com>
2021-11-18 17:30:38 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+
+import NoteBox from "./note-box";
+
+const rootElement = document.getElementById("root");
+render(<NoteBox />, rootElement);
---
+++ src/note-box.tsx
@@ -0,0 +1,3 @@
+export default function NoteBox() {
+  return <div>This is the component</div>;
+}
